b11437a02e4a082dd65745eac79c3008ba078c66
OJ <ollie@email.com>
2021-01-04 22:02:17 +0000

    Country dropdown selector

--- components/checkout/addresses/ShippingAddressCreator.vue
+++ components/checkout/addresses/ShippingAddressCreator.vue
@@ -1,5 +1,7 @@
 <template>
+
   <form action="#" @submit.prevent="store">
+    {{ form }}
     <div class="field">
       <label class="label">Name</label>
       <div class="control">
@@ -35,10 +37,7 @@
           <label class="label">Country</label>
           <div class="control">
             <div class="select is-fullwidth">
-              <!-- <CountryDropdown v-model="form.country_id" /> -->
-              <select>
-                <option value="1">Afganistan</option>
-              </select>
+              <CountryDropdown v-model="form.country_id" />
             </div>
           </div>
         </div>
@@ -57,7 +56,7 @@
 </template>
 
 <script>
-  // import CountryDropdown from '@/components/form/CountryDropdown'
+  import CountryDropdown from '@/components/form/CountryDropdown'
 
   export default {
     data () {
@@ -73,9 +72,9 @@
       }
     },
 
-    // components: {
-    //   CountryDropdown
-    // },
+    components: {
+      CountryDropdown
+    },
 
     methods: {
       async store () {
