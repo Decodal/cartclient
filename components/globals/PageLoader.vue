bcb5ed9d99088078ae197cbab58410a3993649a6
OJ <ollie@email.com>
2021-02-10 20:46:23 +0000

    globals components and new assets

---
+++ components/globals/PageLoader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- Pageloader -->
+    <div class="pageloader"></div>
+    <!-- <div class="infraloader is-active"></div> -->
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <TheAlert />
+    <PageLoader />
+    <MobileNavbar />
+    <ShopSidebar />
+    <FlyingButton />
+    <QuickviewCategories />
+    <QuickviewMain />
+    <QuickviewCart />
     <Navigation />
     <nuxt/>
   </div>
@@ -9,11 +16,26 @@
 <script>
   import Navigation from '@/layouts/partials/Navigation'
   import TheAlert from '@/components/globals/TheAlert'
+  import PageLoader from '@/components/globals/PageLoader'
+  import MobileNavbar from '@/components/globals/MobileNavbar'
+  import ShopSidebar from '@/components/globals/ShopSidebar'
+  import FlyingButton from '@/components/globals/FlyingButton'
+  import QuickviewCategories from '@/components/globals/QuickviewCategories'
+  import QuickviewMain from '@/components/globals/QuickviewMain'
+  import QuickviewCart from '@/components/globals/QuickviewCart'
+
 
   export default {
     components: {
       Navigation,
-      TheAlert
+      TheAlert,
+      PageLoader,
+      MobileNavbar,
+      ShopSidebar,
+      FlyingButton,
+      QuickviewCategories,
+      QuickviewMain,
+      QuickviewCart,
     }
   }
 </script>
