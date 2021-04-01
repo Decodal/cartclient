5e35d488e9ff8dff2a0f9be072aa4b38d18aeb9c
OJ <ollie@email.com>
2021-04-01 17:58:43 +0100

    TopPanel

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <TheAlert />
+    <TopPanel v-if="isShowing" @clicked="onTopPanelClose"></TopPanel>
     <Navigation />
+    
     <nuxt/>
   </div>
 </template>
@@ -9,11 +11,24 @@
 <script>
   import Navigation from '@/layouts/partials/Navigation'
   import TheAlert from '@/components/globals/TheAlert'
+  import TopPanel from '@/layouts/partials/TopPanel'
 
   export default {
     components: {
       Navigation,
-      TheAlert
-    }
+      TheAlert,
+      TopPanel
+    },
+    data() {
+      return {
+        loading: true,
+        isShowing: true
+      }
+    },
+    methods: {
+      onTopPanelClose (value) {
+        this.isShowing = value;
+      }
+    },
   }
 </script>
